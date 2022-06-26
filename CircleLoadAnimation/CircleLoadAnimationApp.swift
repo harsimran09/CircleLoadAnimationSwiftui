42197da68f8130eeec128cb21d76de4a9535b736
Harsimran Kaur <harsimran.kaur1@whitehatjr.com>
2022-06-26 21:44:04 +0530

    Initial Commit

---
+++ CircleLoadAnimation/CircleLoadAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleLoadAnimationApp.swift
+//  CircleLoadAnimation
+//
+//  Created by ITRS-1519 on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CircleLoadAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
